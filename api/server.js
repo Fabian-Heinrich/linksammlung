c09d11dff6986534fbda5cad1ed003da3be84c1b
Fabian <mail@fabian1998.de>
2022-07-01 21:16:59 +0200

    Base structure for backend api with nodejs, express, mongodb. Dev envionrement in docker-compose

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path: '../.env'});
+}
+
+MongoClient.connect(process.env.DATABASE_URL)
+
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3001, () => {
+
+    console.log('Listening on 3001')
+
+})
+
+app.get('/', (req, res) => {
+    res.send("Test!")
+})
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -10,5 +9,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true,
+    port: process.env.FRONTEND_PORT
   }
 })
